81ca9444aa855c2a399f1b2f32444bfefa69551c
Ivan.Vallenas <ivan.vallenas.munoz@gmail.com>
2024-01-01 21:48:26 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './platzi.webp';
 import './App.css';
+import React from 'react';
+
+
+
+const defaultTodos = [
+  {text:'Cortar cebolla', completed:true},{text:'Cortar cebolla', completed:false},{text:'Cortar cebolla', completed:false},{text:"Pollll", completed:false}
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+        <section className='container'>
+          <TodoCounter completed={16} total={25} />
+          <TodoSearch/>
+          </section>
+
+        <section className='container'>
+        <TodoList>
+          {defaultTodos.map(todo => (<TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>))}
+        </TodoList>
+        </section>
+
+        <CreateTodoButton/>
+
+
+    </React.Fragment>
   );
 }
 
-export default App;
+function TodoItem({text,completed}){
+  return(
+    <li className='TodoItem'>
+      <span className={`Icon Icon-check ${completed && "Icon-check--active"}`}>V</span>
+      <p className={`TodoItem-p ${completed && "TodoItem-p--complete"}`}>{text}</p>
+      <span className={`Icon Icon-delete`}>X</span>
+    </li>
+  )
+}
+
+function TodoSearch(){
+  return(
+    <input className='TodoSearch' placeholder='Cortar Cebolla'
+    onChange={(event)=>{console.log(event.target.value)}}
+    />
+  )
+}
+
+function TodoList(props){
+  return(
+    <ul className='TodoList'>
+      {props.children}
+    </ul>
+  )
+}
+
+function CreateTodoButton(){
+  return(
+    <button className='CreateTodoButton'
+    onClick={()=> {console.log("Diste click para agregar tarea")}}
+    >+</button>
+  )
+}
+
+function TodoCounter({total,completed}){
+  return(
+  <h1 className='TodoCounter'>
+    Has completado <span>{completed}</span> de <span>{total}</span>pasos
+  </h1>
+  )
+}
+
+
+
+export {App}
